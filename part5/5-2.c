88a3f72460c392690138e7ff9e2fb888586313c9
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-11-15 18:54:14 +0100

    task 5.2
    extended version B done

--- part5/5-2.c
+++ part5/5-2.c
@@ -4,6 +4,45 @@
 
 #define MAX_WORD_LEN 428 //długość najdłuższego słowa na świecie w transkrypcji na alfabet łaciński.
 
+char* read_string();
+void print_string(char *strings[], int stringsCount);
+void bubbleSort(char *strings[], int stringsCount);
+
+int main(void){
+    int n;
+    scanf("%d", &n);
+    char *strings[n];
+//    DLA WERSJI ROZSZERZONEJ A
+//    for (int i = 0; i < n; ++i) {
+//        strings[i] = read_string();
+//    }
+
+//  DLA WERSJI ROZSZERZONEJ B
+    for (int i = 0; i < n; ++i) {
+        char *buf = read_string();
+        int index = 0;
+        for (int j = 0; j < i-1; ++j) {
+            if(strcmp(strings[j+1], buf)>0){
+                index = j;
+                break;
+            }
+        }
+        for (int j = i; j > index; --j) {
+            strings[j]= strings[j-1];
+        }
+        strings[index]=buf;
+    }
+
+//    DLA WERSJI ROZSZERZONEJ A
+//    bubbleSort(strings, n);
+    print_string(strings, n);
+
+    for (int i = 0; i < n; ++i) {
+        free(strings[i]);
+    }
+    return  0;
+}
+
 char* read_string(){
     char buf[MAX_WORD_LEN];
     scanf("%s", &buf);
@@ -29,21 +68,4 @@ void bubbleSort(char *strings[], int stringsCount){
             }
         }
     }
-}
-
-int main(void){
-    int n;
-    scanf("%d", &n);
-    char *strings[n];
-    for (int i = 0; i < n; ++i) {
-        strings[i] = read_string();
-    }
-
-    bubbleSort(strings, n);
-    print_string(strings, n);
-
-    for (int i = 0; i < n; ++i) {
-        free(strings[i]);
-    }
-    return  0;
 }
