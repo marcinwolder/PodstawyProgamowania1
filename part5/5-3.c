1e80be38ba466aa14a98c3cadab8b2d52e1d4c60
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-11-17 11:35:55 +0100

    5.3 task fix

--- part5/5-3.c
+++ part5/5-3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 #define N 4             // liczba lancuchow
 #define STR_LEN_MAX 100 // maksymalna dlugosc lancucha (liczona z '\0')
@@ -36,13 +37,24 @@ void print_string(char strings[N][STR_LEN_MAX]){
     }
 }
 void bubbleSort(char strings[N][STR_LEN_MAX]){
+    char lowStrings[N][STR_LEN_MAX];
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j <= strlen(strings[i]); ++j) {
+            lowStrings[i][j] = (char)tolower(strings[i][j]);
+        }
+    }
     char buf[STR_LEN_MAX];
+    char lowBuf[STR_LEN_MAX];
     for (int i = 0; i < N; ++i) {
         for (int j = i+1; j < N; ++j) {
-            if(strcmp(strings[i], strings[j])>0){
+            if(strcmp(lowStrings[i], lowStrings[j])>0){
                 strcpy(buf, strings[i]);
                 strcpy(strings[i], strings[j]);
                 strcpy(strings[j], buf);
+
+                strcpy(lowBuf, lowStrings[i]);
+                strcpy(lowStrings[i], lowStrings[j]);
+                strcpy(lowStrings[j], lowBuf);
             }
         }
     }
