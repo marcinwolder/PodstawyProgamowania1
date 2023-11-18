4b94162e2a6ac0bbe82f0fca13fede153622d1be
Folderas <82723806+marcinwolder@users.noreply.github.com>
2023-11-18 23:03:17 +0100

    task 5.1 fix
    new tests were added - prev solution was not right

--- part5/5-1.c
+++ part5/5-1.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 double f(double x);
-double simpson(double min, double max, double delta, double S, int lvl);
-double initSimpson(double min, double max, double delta);
+double simpson(double min, double max, double delta, double S, int lvl, int maxLvl);
+double initSimpson(double min, double max, double delta, int maxLvl);
 
 int main(void) {
     double min, max, delta;
-    printf("Podaj zakres dolny: \n");
+    int maxLvl;
     scanf("%lf", &min);
-    printf("Podaj zakres gorny: \n");
     scanf("%lf", &max);
-    printf("Podaj maksymalny dopuszczalny blad: \n");
     scanf("%lf", &delta);
+    scanf("%d", &maxLvl);
 
-    printf("Przyblizona wartosc to: %lf", initSimpson(min, max, delta));
+    printf("%.3lf", initSimpson(min, max, delta, maxLvl));
 }
 
-double initSimpson(double min, double max, double delta){
+double initSimpson(double min, double max, double delta, int maxLvl){
     double S = (max - min) * (f(min) + 4 * f((min+max)/2) + f(max)) / 6;
-    return simpson(min, max, delta, S, 1);
+    return simpson(min, max, delta, S, 1, maxLvl);
 }
 double f(double x){
     return 1/((x-0.5)*(x-0.5)+0.01);
 }
-double simpson(double min, double max, double delta, double S, int lvl){
-    if (lvl >= 100) return NAN;
+double simpson(double min, double max, double delta, double S, int lvl, int maxLvl){
+    if (lvl >= maxLvl) return NAN;
     double mid = (min+max)/2;
 
     double S1 = (max - mid) * (f(mid) + 4 * f((mid+max)/2) + f(max)) / 6;
     double S2 = (mid - min) * (f(min) + 4 * f((mid+min)/2) + f(mid)) / 6;
 
-    if((S1+S2) - S <= delta) {
-        printf("Poziom rekurencji: %d, dlugosc podprzedzialu: %f\n", lvl, max-min);
+    if(fabs((S1+S2) - S) <= delta) {
         return S1 + S2;
     }
 
-    double simson1 = simpson(mid, max, delta/2, S1, lvl+1);
-    double simson2 = simpson(min, mid, delta/2, S2, lvl+1);
+    double simson1 = simpson(mid, max, delta/2, S1, lvl+1, maxLvl);
+    double simson2 = simpson(min, mid, delta/2, S2, lvl+1, maxLvl);
 
     if(simson1 != NAN && simson2 != NAN) return simson1+simson2;
     else return NAN;
