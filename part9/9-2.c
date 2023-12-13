8772ff2678a9fb9a6da3946a215f1ecafd522ff7
folderas <82723806+marcinwolder@users.noreply.github.com>
2023-12-13 14:40:21 +0100

    Update 9-2.c

--- part9/9-2.c
+++ part9/9-2.c
@@ -7,15 +7,19 @@ typedef struct {
     size_t elSize;
     size_t size;
     size_t capacity;
+    int(*cmp)(const void*, const void*);
+    void(*print)(const void*);
 } Vector;
 
-Vector* init_vector(size_t size, size_t elSize){
+Vector* init_vector(size_t size, size_t elSize, int(*cmp)(const void*, const void*), void(*print)(const void*)){
     Vector* v = (Vector*)malloc(sizeof(Vector));
     void* data = (void*)malloc(size*elSize);
     v->data = data;
     v->elSize = elSize;
     v->size = 0;
     v->capacity = size;
+    v->cmp = cmp;
+    v->print = print;
     return v;
 }
 
@@ -41,9 +45,9 @@ void resize(Vector* v, size_t size){
     }
 }
 
-void dump_data(Vector* v, void(*elPrint)(const void*)){
+void dump_data(Vector* v){
     for(int i = 0; i < v->size; i++){
-        (*elPrint)( ((char*)v->data)+i*v->elSize );
+        (*v->print)( ((char*)v->data)+i*v->elSize );
     }
     printf("\n");
 }
@@ -69,21 +73,21 @@ void insert(Vector* v, const size_t index, const void* val){
 }
 
 void erase(Vector* v, const size_t index){
+    char* elementAddr = (char*)(v->data)+index*v->elSize;
     if(index+1==v->size){
-        char* elementAddr = (char*)(v->data)+index*v->elSize;
         free(elementAddr);
         memset( elementAddr, 0, v->elSize );
     }
     else {
-        char* elementAddr = (char*)(v->data)+index*v->elSize;
+        // free(elementAddr); -> Error: Double free
         memmove(elementAddr, elementAddr+v->elSize, (v->size-index-1)*v->elSize);
     }
     v->size--;
 }
 
-void erase_value(Vector* v, const void* val, int(*cmp)(const void*, const void*)){
+void erase_value(Vector* v, const void* val){
     for(int i = 0; i < v->size; i++){
-        if(!(*cmp)(val, (char*)(v->data)+i*v->elSize)){
+        if(!(*v->cmp)(val, (char*)(v->data)+i*v->elSize)){
             erase(v, i);
         }
     }
@@ -100,9 +104,19 @@ int cmpDouble(const void* el1, const void* el2){
     return *(double*)el1 - *(double*)el2;
 }
 
+void printString(const void* el){
+    char *str = *(char**)el;
+    printf("%s ", str);
+}
+int cmpString(const void* el1, const void* el2){
+    char *str1 = *(char**)el1;
+    char *str2 = *(char**)el2;
+    return strcmp(str1, str2);
+}
+
 int main(void){
     // zdefiniowany wektor V1, w którym zapisywane są liczby typu double, początkowa pojemność wystarcza na zapisania 5 liczb,
-    Vector *v = init_vector(5, sizeof(double));
+    Vector *v = init_vector(5, sizeof(double), cmpDouble, printDouble);
     
     // wywoływane są funkcje (w podanej kolejności), 
     // - 2 razy push_front() z danymi 1. i 2.,
@@ -126,10 +140,45 @@ int main(void){
     // - jeżeli zostały zdefiniowane funkcje 8-11, to należy je także wywołać,
     insert(v, 2, &f);
     erase(v, 4);
-    erase_value(v, &f, cmpDouble);
+    erase_value(v, &f);
     shrink_to_fit(v);
     
     // - dump_data().
-    dump_data(v, printDouble);
+    dump_data(v);
+    
+    // W tym samym programie (używając tych samych funkcji ogólnego przeznaczenia) zdefiniować drugą strukturę Vector V2, ale przeznaczoną dla danych typu łańcucha znaków. \
+    
+    printf("\n===\n\n");
+    
+    Vector *v2 = init_vector(5, sizeof(char*), cmpString, printString);
+    
+    // Wykonać podobną sekwencję wywołań funkcji obsługujących wektor V2.
+    
+    char *a2 = "a";
+    char *b2 = "b";
+    push_front(v2, &a2);
+    push_front(v2, &b2);
+    
+    // - 4 razy push_back() z danymi 10., 2., 14., 16.,
+    char *c2 = "c";
+    char *d2 = "d";
+    char *e2 = "e";
+    push_back(v2, &c2);
+    push_back(v2, &b2);
+    push_back(v2, &d2);
+    push_back(v2, &e2);
+    // - 1 raz push_front() z daną 100.,
+    char *f2 = "f";
+    push_front(v2, &f2);
+    
+    // - jeżeli zostały zdefiniowane funkcje 8-11, to należy je także wywołać,
+    insert(v2, 2, &f2);
+    erase(v2, 4);
+    erase_value(v2, &f2);
+    shrink_to_fit(v2);
+    
+    // - dump_data().
+    dump_data(v2);
+    
     return 0;
 }
